openinx/incubator-iceberg

Convert the flink datatype to iceberg data type and vice versa (#6)
* Convert the flink datatype to iceberg data type and vice versa

* align the flink data type with iceberg data type in write path

* Let FlinkParquetWriters#WriterBuilder to extend the GenericParquetWriter#WriterBuilder

* Add the flink parquet readers

data/src/main/java/org/apache/iceberg/data/parquet/GenericParquetReaders.java
@@ -85,8 +85,8 @@ public class GenericParquetReaders {
     }
   }
 
-  private static class FallbackReadBuilder extends ReadBuilder {
-    FallbackReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
+  public static class FallbackReadBuilder extends ReadBuilder {
+    public FallbackReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
       super(type, idToConstant);
     }
 
@@ -116,11 +116,11 @@ public class GenericParquetReaders {
     }
   }
 
-  private static class ReadBuilder extends TypeWithSchemaVisitor<ParquetValueReader<?>> {
+  public static class ReadBuilder extends TypeWithSchemaVisitor<ParquetValueReader<?>> {
     private final MessageType type;
     private final Map<Integer, ?> idToConstant;
 
-    ReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
+    public ReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
       this.type = type;
       this.idToConstant = idToConstant;
     }
@@ -294,8 +294,12 @@ public class GenericParquetReaders {
       }
     }
 
-    MessageType type() {
+    protected MessageType type() {
       return type;
+    }
+
+    protected Map<Integer, ?> idToConstant() {
+      return idToConstant;
     }
 
     private String[] currentPath() {

flink/src/main/java/org/apache/iceberg/flink/data/FlinkParquetReaders.java
@@ -19,5 +19,153 @@
 
 package org.apache.iceberg.flink.data;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import java.util.List;
+import java.util.Map;
+import org.apache.flink.types.Row;
+import org.apache.iceberg.Schema;
+import org.apache.iceberg.data.parquet.GenericParquetReaders;
+import org.apache.iceberg.parquet.ParquetSchemaUtil;
+import org.apache.iceberg.parquet.ParquetValueReader;
+import org.apache.iceberg.parquet.ParquetValueReaders;
+import org.apache.iceberg.parquet.TypeWithSchemaVisitor;
+import org.apache.iceberg.types.Types;
+import org.apache.parquet.schema.GroupType;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.Type;
+
 public class FlinkParquetReaders {
+  private FlinkParquetReaders() {
+
+  }
+
+  public static ParquetValueReader<Row> buildReader(Schema expectedSchema,
+                                                    MessageType fileSchema) {
+    return buildReader(expectedSchema, fileSchema, ImmutableMap.of());
+  }
+
+  @SuppressWarnings("unchecked")
+  public static ParquetValueReader<Row> buildReader(Schema expectedSchema,
+                                                    MessageType fileSchema,
+                                                    Map<Integer, ?> idToConstant) {
+    if (ParquetSchemaUtil.hasIds(fileSchema)) {
+      return (ParquetValueReader<Row>)
+          TypeWithSchemaVisitor.visit(expectedSchema.asStruct(), fileSchema,
+              new ReadBuilder(fileSchema, idToConstant));
+    } else {
+      return (ParquetValueReader<Row>)
+          TypeWithSchemaVisitor.visit(expectedSchema.asStruct(), fileSchema,
+              new FallbackReadBuilder(fileSchema, idToConstant));
+    }
+  }
+
+  private static class FallbackReadBuilder extends GenericParquetReaders.FallbackReadBuilder {
+
+    FallbackReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
+      super(type, idToConstant);
+    }
+
+    @Override
+    public ParquetValueReader<?> struct(Types.StructType expected, GroupType struct,
+                                        List<ParquetValueReader<?>> fieldReaders) {
+      // the expected struct is ignored because nested fields are never found when the
+      List<ParquetValueReader<?>> newFields = Lists.newArrayListWithExpectedSize(
+          fieldReaders.size());
+      List<Type> types = Lists.newArrayListWithExpectedSize(fieldReaders.size());
+      List<Type> fields = struct.getFields();
+      for (int i = 0; i < fields.size(); i += 1) {
+        Type fieldType = fields.get(i);
+        int fieldD = type().getMaxDefinitionLevel(path(fieldType.getName())) - 1;
+        newFields.add(ParquetValueReaders.option(fieldType, fieldD, fieldReaders.get(i)));
+        types.add(fieldType);
+      }
+      return new RowReader(types, newFields, expected);
+    }
+  }
+
+  private static class ReadBuilder extends GenericParquetReaders.ReadBuilder {
+
+    ReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
+      super(type, idToConstant);
+    }
+
+    @Override
+    public ParquetValueReader<?> struct(Types.StructType expected, GroupType struct,
+                                        List<ParquetValueReader<?>> fieldReaders) {
+      // match the expected struct's order
+      Map<Integer, ParquetValueReader<?>> readersById = Maps.newHashMap();
+      Map<Integer, Type> typesById = Maps.newHashMap();
+      List<Type> fields = struct.getFields();
+      for (int i = 0; i < fields.size(); i += 1) {
+        Type fieldType = fields.get(i);
+        int fieldD = type().getMaxDefinitionLevel(path(fieldType.getName())) - 1;
+        int id = fieldType.getId().intValue();
+        readersById.put(id, ParquetValueReaders.option(fieldType, fieldD, fieldReaders.get(i)));
+        typesById.put(id, fieldType);
+      }
+
+      List<Types.NestedField> expectedFields = expected != null ?
+          expected.fields() : ImmutableList.of();
+      List<ParquetValueReader<?>> reorderedFields = Lists.newArrayListWithExpectedSize(
+          expectedFields.size());
+      List<Type> types = Lists.newArrayListWithExpectedSize(expectedFields.size());
+      for (Types.NestedField field : expectedFields) {
+        int id = field.fieldId();
+        if (idToConstant().containsKey(id)) {
+          // containsKey is used because the constant may be null
+          reorderedFields.add(ParquetValueReaders.constant(idToConstant().get(id)));
+          types.add(null);
+        } else {
+          ParquetValueReader<?> reader = readersById.get(id);
+          if (reader != null) {
+            reorderedFields.add(reader);
+            types.add(typesById.get(id));
+          } else {
+            reorderedFields.add(ParquetValueReaders.nulls());
+            types.add(null);
+          }
+        }
+      }
+
+      return new RowReader(types, reorderedFields, expected);
+    }
+  }
+
+  static class RowReader extends ParquetValueReaders.StructReader<Row, Row> {
+    private final Types.StructType structType;
+
+    RowReader(List<Type> types,
+              List<ParquetValueReader<?>> readers,
+              Types.StructType struct) {
+      super(types, readers);
+      this.structType = struct;
+    }
+
+    @Override
+    protected Row newStructData(Row reuse) {
+      if (reuse != null) {
+        return reuse;
+      } else {
+        return new Row(structType.fields().size());
+      }
+    }
+
+    @Override
+    protected Object getField(Row row, int pos) {
+      return row.getField(pos);
+    }
+
+    @Override
+    protected Row buildStruct(Row row) {
+      return row;
+    }
+
+    @Override
+    protected void set(Row row, int pos, Object value) {
+      row.setField(pos, value);
+    }
+  }
 }

YesOrNo828/iceberg

data/src/main/java/org/apache/iceberg/data/parquet/GenericParquetReaders.java
@@ -84,8 +84,8 @@ public class GenericParquetReaders {
     }
   }
 
-  private static class FallbackReadBuilder extends ReadBuilder {
-    FallbackReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
+  public static class FallbackReadBuilder extends ReadBuilder {
+    public FallbackReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
       super(type, idToConstant);
     }
 
@@ -115,13 +115,17 @@ public class GenericParquetReaders {
     }
   }
 
-  private static class ReadBuilder extends TypeWithSchemaVisitor<ParquetValueReader<?>> {
+  public static class ReadBuilder extends TypeWithSchemaVisitor<ParquetValueReader<?>> {
     private final MessageType type;
     private final Map<Integer, ?> idToConstant;
 
-    ReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
+    public ReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
       this.type = type;
       this.idToConstant = idToConstant;
+    }
+
+    protected Map<Integer, ?> getIdToConstant() {
+      return idToConstant;
     }
 
     @Override
@@ -293,7 +297,7 @@ public class GenericParquetReaders {
       }
     }
 
-    MessageType type() {
+    protected MessageType type() {
       return type;
     }
   }

flink/src/main/java/org/apache/iceberg/flink/data/FlinkParquetReaders.java
@@ -19,5 +19,153 @@
 
 package org.apache.iceberg.flink.data;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import java.util.List;
+import java.util.Map;
+import org.apache.flink.types.Row;
+import org.apache.iceberg.Schema;
+import org.apache.iceberg.data.parquet.GenericParquetReaders;
+import org.apache.iceberg.parquet.ParquetSchemaUtil;
+import org.apache.iceberg.parquet.ParquetValueReader;
+import org.apache.iceberg.parquet.ParquetValueReaders;
+import org.apache.iceberg.parquet.TypeWithSchemaVisitor;
+import org.apache.iceberg.types.Types;
+import org.apache.parquet.schema.GroupType;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.Type;
+
 public class FlinkParquetReaders {
+  private FlinkParquetReaders() {
+
+  }
+
+  public static ParquetValueReader<Row> buildReader(Schema expectedSchema,
+                                                    MessageType fileSchema) {
+    return buildReader(expectedSchema, fileSchema, ImmutableMap.of());
+  }
+
+  @SuppressWarnings("unchecked")
+  public static ParquetValueReader<Row> buildReader(Schema expectedSchema,
+                                                    MessageType fileSchema,
+                                                    Map<Integer, ?> idToConstant) {
+    if (ParquetSchemaUtil.hasIds(fileSchema)) {
+      return (ParquetValueReader<Row>)
+          TypeWithSchemaVisitor.visit(expectedSchema.asStruct(), fileSchema,
+              new ReadBuilder(fileSchema, idToConstant));
+    } else {
+      return (ParquetValueReader<Row>)
+          TypeWithSchemaVisitor.visit(expectedSchema.asStruct(), fileSchema,
+              new FallbackReadBuilder(fileSchema, idToConstant));
+    }
+  }
+
+  private static class FallbackReadBuilder extends GenericParquetReaders.FallbackReadBuilder {
+
+    FallbackReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
+      super(type, idToConstant);
+    }
+
+    @Override
+    public ParquetValueReader<?> struct(Types.StructType expected, GroupType struct,
+                                        List<ParquetValueReader<?>> fieldReaders) {
+      // the expected struct is ignored because nested fields are never found when the
+      List<ParquetValueReader<?>> newFields = Lists.newArrayListWithExpectedSize(
+          fieldReaders.size());
+      List<Type> types = Lists.newArrayListWithExpectedSize(fieldReaders.size());
+      List<Type> fields = struct.getFields();
+      for (int i = 0; i < fields.size(); i += 1) {
+        Type fieldType = fields.get(i);
+        int fieldD = type().getMaxDefinitionLevel(path(fieldType.getName())) - 1;
+        newFields.add(ParquetValueReaders.option(fieldType, fieldD, fieldReaders.get(i)));
+        types.add(fieldType);
+      }
+      return new RowReader(types, newFields, expected);
+    }
+  }
+
+  private static class ReadBuilder extends GenericParquetReaders.ReadBuilder {
+
+    ReadBuilder(MessageType type, Map<Integer, ?> idToConstant) {
+      super(type, idToConstant);
+    }
+
+    @Override
+    public ParquetValueReader<?> struct(Types.StructType expected, GroupType struct,
+                                        List<ParquetValueReader<?>> fieldReaders) {
+      // match the expected struct's order
+      Map<Integer, ParquetValueReader<?>> readersById = Maps.newHashMap();
+      Map<Integer, Type> typesById = Maps.newHashMap();
+      List<Type> fields = struct.getFields();
+      for (int i = 0; i < fields.size(); i += 1) {
+        Type fieldType = fields.get(i);
+        int fieldD = type().getMaxDefinitionLevel(path(fieldType.getName())) - 1;
+        int id = fieldType.getId().intValue();
+        readersById.put(id, ParquetValueReaders.option(fieldType, fieldD, fieldReaders.get(i)));
+        typesById.put(id, fieldType);
+      }
+
+      List<Types.NestedField> expectedFields = expected != null ?
+          expected.fields() : ImmutableList.of();
+      List<ParquetValueReader<?>> reorderedFields = Lists.newArrayListWithExpectedSize(
+          expectedFields.size());
+      List<Type> types = Lists.newArrayListWithExpectedSize(expectedFields.size());
+      for (Types.NestedField field : expectedFields) {
+        int id = field.fieldId();
+        if (getIdToConstant().containsKey(id)) {
+          // containsKey is used because the constant may be null
+          reorderedFields.add(ParquetValueReaders.constant(getIdToConstant().get(id)));
+          types.add(null);
+        } else {
+          ParquetValueReader<?> reader = readersById.get(id);
+          if (reader != null) {
+            reorderedFields.add(reader);
+            types.add(typesById.get(id));
+          } else {
+            reorderedFields.add(ParquetValueReaders.nulls());
+            types.add(null);
+          }
+        }
+      }
+
+      return new RowReader(types, reorderedFields, expected);
+    }
+  }
+
+  static class RowReader extends ParquetValueReaders.StructReader<Row, Row> {
+    private final Types.StructType structType;
+
+    RowReader(List<Type> types,
+              List<ParquetValueReader<?>> readers,
+              Types.StructType struct) {
+      super(types, readers);
+      this.structType = struct;
+    }
+
+    @Override
+    protected Row newStructData(Row reuse) {
+      if (reuse != null) {
+        return reuse;
+      } else {
+        return new Row(structType.fields().size());
+      }
+    }
+
+    @Override
+    protected Object getField(Row row, int pos) {
+      return row.getField(pos);
+    }
+
+    @Override
+    protected Row buildStruct(Row row) {
+      return row;
+    }
+
+    @Override
+    protected void set(Row row, int pos, Object value) {
+      row.setField(pos, value);
+    }
+  }
 }